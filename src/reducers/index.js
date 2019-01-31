eec95236408fb937428906294a74726e7c9fe76b
mitsuyoshi <m123matsuo@icloud.com>
2019-01-31 19:12:38 +0900

    implement event new component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 //全てのreducerを結合して状態管理するファイル
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import events from './events';
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
