3ee19dbf562bdfb4a740efba1119aea78f9dbef3
mitsuyoshi <m123matsuo@icloud.com>
2019-01-31 16:55:12 +0900

    implement events index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 //全てのreducerを結合して状態管理するファイル
 import { combineReducers } from 'redux';
-import count from './count';
+import events from './events';
 
-export default combineReducers({ count })
+export default combineReducers({ events })
